57a5841b22327c9f8a4feca60f279f1fe8fbd5f4
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-06-14 02:03:42 +0900

    test: UIでコケる数値のテストを仕込む。

--- test/domain/langtonsloops/LangtonsLoops.spec.ts
+++ test/domain/langtonsloops/LangtonsLoops.spec.ts
@@ -19,4 +19,24 @@ describe("LangtonsLoopsTest", () => {
         expect(sut.lives.length).toEqual(512);
         expect(sut.lives[0].length).toEqual(512);
     });
+
+    test("世界の大きさ(size)指定で最小値で初期化が出来る。", () => {
+        const sut = new LangtonsLoops();
+
+        sut.langtonsLoops(64);
+
+        expect(sut.lives.length).toEqual(64);
+        expect(sut.lives[0].length).toEqual(64);
+    });
+
+    test("世界の大きさ(size)指定でいくつかの特徴的な値で初期化が出来る。", () => {
+        const sut = new LangtonsLoops();
+
+        [100, 200, 500, 999].forEach(size => {
+            sut.langtonsLoops(size);
+
+            expect(sut.lives.length).toEqual(size);
+            expect(sut.lives[0].length).toEqual(size);
+        });
+    });
 });
